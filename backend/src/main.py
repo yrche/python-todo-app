5e854f0e2f3175de096224e4b304e0c83875b63a
yrche <lunicx.work@gmail.com>
2026-07-21 13:06:03 +0200

    feat(server): configure CORS middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.v1.endpoints.TaskRouter import router as task_router
 from src.infrastructure.database.InitializeSession import engine
 from src.infrastructure.database.models.BaseModel import Base
 from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+origin_url = os.getenv("ORIGIN_URL")
+
+if not origin_url:
+    raise ValueError("ORIGIN_URL environment variable is not set")
 
 
 async def setup_database():
@@ -17,4 +26,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origin_url],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(task_router)
