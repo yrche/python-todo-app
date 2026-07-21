5455d471fbd775b0da34f46b2f817f25d8e27195
yrche <lunicx.work@gmail.com>
2026-07-21 19:07:58 +0200

    fix(config): added outputs to next config file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
